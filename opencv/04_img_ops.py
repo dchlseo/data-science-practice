c4ab9a77d969a258856cea904d6d401353fa7cb8
SDC <70427747+dchlseo@users.noreply.github.com>
2023-11-08 13:53:19 +0900

    initializing imported images

---
+++ opencv/04_img_ops.py
@@ -0,0 +1,31 @@
+﻿import numpy as np
+import cv2
+
+'''
+# 새 영상 생성하기
+img1 = np.empty((240, 320), dtype=np.uint8)       # grayscale image
+img2 = np.zeros((240, 320, 3), dtype=np.uint8)    # color image
+img3 = np.ones((240, 320), dtype=np.uint8) * 255  # white
+img4 = np.full((240, 320, 3), (0, 255, 255), dtype=np.uint8)  # yellow
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.imshow('img3', img3)
+cv2.imshow('img4', img4)
+cv2.waitKey()
+cv2.destroyAllWindows()
+'''
+# 영상 복사
+img1 = cv2.imread('HappyFish.jpg')
+
+img2 = img1
+img3 = img1.copy()
+
+img1[:, :, :] = 255 # this will affect both img1 and img2 (shallow copied version).
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.imshow('img3', img3)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
